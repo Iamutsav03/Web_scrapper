1468ff5d626c6b0cb85ad755207e301bcc5a1293
Utsav Singh <utsav3711@gmail.com>
2026-03-07 02:23:34 +0530

    route updated

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 const scraperRoutes = require("./routes/scraperRoutes");
+const gmbRoutes = require("./routes/googleMapsRoutes");
 const errorHandler = require("./utils/errorHandler");
 
 const app = express();
@@ -9,6 +10,7 @@ app.use(cors());
 app.use(express.json());
 
 app.use("/api", scraperRoutes);
+app.use("/api", gmbRoutes);
 
 app.get("/", (req, res) => {
     res.json({ message: "Web Scraper Platform API Running" });
